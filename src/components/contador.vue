b0aeab7512f4a6524eef7075502b986f9c545d48
Oscar <oscar.paco.vasquez@gmail.com>
2026-07-08 21:03:25 -0400

    uso de variables reactivas y creacción de componente contador

---
+++ src/components/contador.vue
@@ -0,0 +1,21 @@
+<template>
+ <h1>hola mundo</h1> <br><br><br>
+{{ contador }} <br><br><br>
+<button @click="incrementar">Incrementar</button>
+<button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+}
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+
+</style>
